1ba43f59515ad8bdda460d77ce5a1f2c05945a7b
wadieab <wadieabuissa@gmail.com>
2021-06-15 20:33:09 +0300

    add usr fix

--- src/loginComponents/LoginForm.jsx
+++ src/loginComponents/LoginForm.jsx
@@ -33,13 +33,11 @@ class LoginForm extends React.Component {
 
   onLoginSuccess() {
     console.log("Successful login");
-
   }
 
   componentDidUpdate(prevProp) {
     if (this.props.isLoggedIn !== prevProp.isLoggedIn) {
       this.setState({loading: false});
-   console.log("in")
     }
   }
 
@@ -62,6 +60,7 @@ console.log(this.state)
     const username = this.state.username;
     const password = this.state.password;
     this.setState({ loading: true });
+    this.props.funcret(password)
     firebase
       .auth()
       .signInWithEmailAndPassword(username, password)
